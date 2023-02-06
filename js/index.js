2bbe59884f9e729fb517fd92eb885d91b2b8c9d4
SunmoonL <dltjsans123@naver.com>
2023-02-06 15:55:22 +0900

    23.02.06

--- js/index.js
+++ js/index.js
@@ -59,6 +59,7 @@ icons.forEach (icon => {
 });
 });
 
+//map
 $(function(){
   $("#map > div").mouseenter(function(){
     $(this).children().stop().fadeIn();
@@ -69,48 +70,18 @@ $(function(){
   });
 });
 
-$(function(){
-$("#map > div").mousemove(function(event) {
-  var x = event.pageX;
-  var y = event.pageY;
-  // if(70 < x < 315, 50 < y < 155){
-  //   $("#vid").attr("src","img/America.mp4")}
-  // else if(255 < x < 450, 205 < y < 475){
-  //   $("#vid").attr("src","img/southAmerica.mp4")
-  // }
-  // else if(500 < x < 545, -68 < y < -44){
-  //   $("#vid").attr("src","img/Iceland.mp4")
-  // }
-  // else if(560 < x < 730, -120 < y < 110){
-  //   $("#vid").attr("src","img/Europe.mp4")
-  // }
-  // else if(995 < x < 1200, 150 < y < 290){
-  //   $("#vid").attr("src","img/Asia.mp4")
-  // }
-  // else if(780 < x < 1410, -290 < y < 80){
-  //   $("#vid").attr("src","img/Russia.mp4")
-  // };
+var arr=["img/America.mp4","img/southAmerica.mp4","img/Europe.mp4","img/Iceland.mp4","img/Asia.mp4","img/Russia.mp4"]
 
-  if($(this)==$("#america")){
-    $("#vid").attr("src","img/America.mp4")}
+$(function () {
+  $("#map > div").mouseenter(function (event) {
+    var x = event.pageX;
+    var y = event.pageY;
+    var i = $(this).index();
+    $("#vid").attr("src", arr[i - 1]);
 
-  else if($(this)==$("#southAmerica")){
-    $("#vid").attr("src","img/southAmerica.mp4")}
-
-  else if($(this)==$("#europe")){
-    $("#vid").attr("src","img/Europe.mp4")}
-
-  else if($(this)==$("#iceland")){
-    $("#vid").attr("src","img/Iceland.mp4")}
-
-  else if($(this)==$("#eastAsia")){
-    $("#vid").attr("src","img/Asia.mp4")}
-
-  else if($(this)==$("#russia")){
-    $("#vid").attr("src","img/Russia.mp4")};
-
-  $("#vid").css({left: x - 150, top: y - 450}).addClass("on");
-}).mouseleave(function() {
-  $("#vid").removeClass("on");
-});
+    $("#vid").css({ left: x - 150, top: y - 450 }).addClass("on");
+  }).mouseleave(function () {
+    $("#vid").removeClass("on");
+  });
+  
 });
